05692465b3a3176b5af480b861c31fe597dae500
FernandoAlonso-1981 <contato@vovosolucoes.com.br>
2023-05-14 23:59:11 -0300

    iniciando a página whatsapp

--- compartilhar.js
+++ compartilhar.js
@@ -1,4 +1,5 @@
 function texto() {
+    window.alert('QR code')
     const foco1 = String(document.getElementById("recebelink").value) //transformar o obejto html em string
     const divResultado1 = document.getElementById("resultado1")
     const divResultado2 = document.getElementById("resultado2")
@@ -61,6 +62,84 @@ function texto() {
     
     gerarPagina2(texto, nomeArquivo+"___qrcode.html")
 }
+
+
+
+
+function whatsApp() {
+    window.alert('WhatsApp')
+    window.alert('QR code')
+    const foco1 = String(document.getElementById("recebelink").value) //transformar o obejto html em string
+    const divResultado1 = document.getElementById("resultado1")
+    const divResultado2 = document.getElementById("resultado2")
+
+    
+    
+    const texto = divResultado1.innerHTML =  `
+                                            <!DOCTYPE html>
+                                            <html lang="pt-br">
+                                            <head>
+                                                <meta charset="UTF-8">
+                                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                                <link rel="shortcut icon" href="https://cartaodevisitavirtual.com.br/wp-content/uploads/sites/32/2022/12/vovo.ico" type="image/x-icon">
+                                                <style> 
+                                                    * {
+                                                        padding: 0px;
+                                                        margin: 0px;
+                                                        
+                                                        color: #484848;
+                                                        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                                                            
+                                                        box-sizing: border-box;
+                                                        font-display: swap;
+                                                    }
+                                                    html, body {
+                                                        background-color: #f7f7f7;
+                                                        margin: auto;   
+                                                    }
+                                                    h1,h2,p,div{
+
+                                                        padding: 1em;
+                                                        text-align: center;
+                                                        
+                                                    }
+                                                    img#logo{
+                                                        max-width: 80%;
+                                                        max-height: 80%;
+                                                    }
+                                                </style>
+                                                <title>Cartão de Visita Virtual</title>
+                                            </head>
+                                            <body>
+                                                <h1 id="h1">Cartão de Visita Virtual</h1>
+                                                <h2 >Compartilhe via <strong>WhatsApp<strong></h2>
+                                                
+                                                <div>
+                                                    <input style="height: 40px; width:50%;" name="tel" id="telefone"   placeholder=" Celular com DDD * (Obrigatório)" >
+                                                </div>
+                                                <div>
+                                                    <input style="height: 40px; width:50%;" name="tel" id="mensagem"   placeholder=" Mensagem Padrão (Opcional)" >
+                                                </div>
+                                                <div style="padding-top: 100px;"> <a href="https://cartaovirtualdevisita.com.br/" target="_blank"><img id="logo" src="https://cdn.fernandoalonso.hosting.dousvies.com/wp-content/uploads/sites/32/2021/12/31002033/logotipo-Cartao-de-Visita-Virtual.png" alt="Cartão de Visita Virtual"></a> </div>
+
+                                            </body>
+                                            </html>
+                                        
+                                        `
+
+    const date = new Date().toLocaleString();
+    const nomeArquivo = date
+    
+    gerarPagina2(texto, nomeArquivo+"___whatsapp.html")
+}
+
+
+
+
+
+
+
 
 //gerar o arquivo no computador
 function gerarPagina2(content=texto, filename="qrcode.html", contentType){
