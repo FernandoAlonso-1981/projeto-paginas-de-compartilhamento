28ee6f38aacace24bd2c5746cdd2ca2cfc79a31b
FernandoAlonso-1981 <contato@vovosolucoes.com.br>
2023-05-14 22:34:03 -0300

    página Qrcode pronta

--- compartilhar.js
+++ compartilhar.js
@@ -1,18 +1,84 @@
-function gerarPagina(){
-    const foco = String(document.getElementById("recebelink").value) //transformar o obejto html em strinfo
+function texto() {
+    const foco1 = String(document.getElementById("recebelink").value) //transformar o obejto html em string
     const divResultado1 = document.getElementById("resultado1")
     const divResultado2 = document.getElementById("resultado2")
-    
-    divResultado1.innerHTML = `<p>Deu certo o resultado = ${foco}</p>`
 
-    //Gerar página compartilhamento por link
+    let qrcode = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${foco1}`;
+    
+    const texto = divResultado1.innerHTML =  `
+                                            <!DOCTYPE html>
+                                            <html lang="pt-br">
+                                            <head>
+                                                <meta charset="UTF-8">
+                                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                                <link rel="shortcut icon" href="https://cartaodevisitavirtual.com.br/wp-content/uploads/sites/32/2022/12/vovo.ico" type="image/x-icon">
+                                                <style> 
+                                                    * {
+                                                        padding: 0px;
+                                                        margin: 0px;
+                                                        
+                                                        color: #484848;
+                                                        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                                                            
+                                                        box-sizing: border-box;
+                                                        font-display: swap;
+                                                    }
+                                                    html, body {
+                                                        background-color: #f7f7f7;
+                                                        margin: auto;   
+                                                    }
+                                                    h1,h2,p,div{
 
-    divResultado2.innerHTML =   `<h1>Comparilhe via QR Code</h1>
-                                <h2>Cartão de Visita Virtual</h2>
-                                <p>Aqui o QR Code + o ${foco}<p>
-                                <img src="https://cdn.fernandoalonso.hosting.dousvies.com/wp-content/uploads/sites/32/2021/12/31002033/logotipo-Cartao-de-Visita-Virtual.png" alt="Cartão de Visita Virtual">
-                                `
+                                                        padding: 1em;
+                                                        text-align: center;
+                                                        
+                                                    }
+                                                    img#logo{
+                                                        max-width: 80%;
+                                                        max-height: 80%;
+                                                    }
+                                                    img#qrcode{
+                                                        
+                                                        
+                                                        border: 10px solid #ffffff;
+                                                    }
+                                                </style>
+                                                <title>Cartão de Visita Virtual</title>
+                                            </head>
+                                            <body>
+                                                <h1 id="h1">Cartão de Visita Virtual</h1>
+                                                <h2 >Compartilhe via <strong>QR Code<strong></h2>
+                                                <div><img id="qrcode"src="${qrcode}" alt=""></div>
+                                                <div> <a href="https://cartaovirtualdevisita.com.br/" target="_blank"><img id="logo" src="https://cdn.fernandoalonso.hosting.dousvies.com/wp-content/uploads/sites/32/2021/12/31002033/logotipo-Cartao-de-Visita-Virtual.png" alt="Cartão de Visita Virtual"></a> </div>
+                                            </body>
+                                            </html>
+                                        
+                                        `
+
+    window.alert('2--->' +foco1) 
     
-  
-    
+    gerarPagina2(texto,"qrcode.html")
 }
+
+//gerar o arquivo no computador
+function gerarPagina2(content=texto, filename="qrcode.html", contentType){
+
+    window.alert('3--->'+texto)
+
+    if(!contentType){
+        contentType = 'application/octet-stream';
+    }
+    var a = document.createElement('a');
+    var blob = new Blob([content], {'type':contentType});
+    a.href = window.URL.createObjectURL(blob);
+    a.download = filename;
+    a.click();
+    
+    //navigator.clipboard.writeText(texto) //copia para a area de trabalho                     
+
+}
+
+
+
+
---
+++ teste.js
@@ -0,0 +1,39 @@
+function download(content, filename, contentType){
+    if(!contentType){
+        contentType = 'application/octet-stream';
+    }
+    var a = document.createElement('a');
+    var blob = new Blob([content], {'type':contentType});
+    a.href = window.URL.createObjectURL(blob);
+    a.download = filename;
+    a.click();
+}
+
+
+
+//gerar qr code
+let qrcode = new QRCode("qrcode");
+
+function makeCode () {		
+	var elText = document.getElementById("text");
+	
+	if (!elText.value) {
+		alert("Input a text");
+		elText.focus();
+		return;
+	}
+	
+	qrcode.makeCode(elText.value);
+}
+
+makeCode();
+
+$("#text").
+	on("blur", function () {
+		makeCode();
+	}).
+	on("keydown", function (e) {
+		if (e.keyCode == 13) {
+			makeCode();
+		}
+	});
